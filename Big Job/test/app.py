f8c6edf59d3fed4b55b7ea55cde05385c32d7e89
AurelPap <apapillon@aurelpap.fr>
2024-01-05 14:26:57 +0100

    Step 6

--- Big Job/test/app.py
+++ Big Job/test/app.py
@@ -3,40 +3,31 @@ import sqlite3
 
 app = Flask(__name__)
 
-# Path to your SQLite database file
-DATABASE = './data/CarbonFootprint.db'
-# DATABASE = '../local_CarbonFootprint.db'
 
-def query_database(query):
-    # try:
-    conn = sqlite3.connect(DATABASE)
-    c = conn.cursor()
-    c.execute(query)
-    data = c.fetchall()
-    # conn.close()
-    return data
-    # except sqlite3.Error as e:
-    #     return f"An error occurred: {e}"
+def get_db_connection():
+    conn = sqlite3.connect('./data/CarbonFootprint.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 @app.route('/')
 def index():
-    # Example query
-    # query = "SELECT * FROM Country WHERE Country LIKE '%Germany%'"
-    
-    import os
+    conn = get_db_connection()
+    query_result = conn.execute('SELECT * FROM country LIMIT 10').fetchall()
+    cols = conn.execute(f"PRAGMA table_info({'country'})").fetchall()
+    countries = conn.execute('SELECT Country FROM country').fetchall()
+    conn.close()
+    return render_template('index.html', query_result=query_result, cols=cols, countries=countries)
 
-    print("Current working directory:", os.getcwd())
-    directory = os.getcwd()
-    
-    table_name = 'World'
-    query = f"SELECT * FROM {table_name}"
-    query_result = query_database(query) 
-    cols = query_database(f"PRAGMA table_info({table_name});")
 
-    # if isinstance(query_result, str):
-    #     # If there's an error, display the error message
-    #     return query_result
-    return render_template('index.html', directory = directory, query_result=query_result, cols = cols)
+@app.route('/update_content/<selected_country>')
+def update_content(selected_country):
+    conn = get_db_connection()
+    result = conn.execute(f'SELECT * FROM country WHERE Country = "{selected_country}"').fetchall()
+    cols = conn.execute(f"PRAGMA table_info({'country'})").fetchall()
+    conn.close()
+    return render_template('content_partial.html', query_result=result, cols=cols)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Big Job/test/test.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('./data/CarbonFootprint.db')
-cur = conn.cursor()
-
-cur.execute("SELECT * FROM World")
-rows = cur.fetchall()
-print(rows[4])
